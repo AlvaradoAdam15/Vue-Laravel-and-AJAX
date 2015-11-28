6ba7a37c0bb2b3a97c505e97ae8475ad14856568
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-11-28 12:45:33 +0100

    Show Tasks in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,43 +3,23 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootsrap/3.3.4/bootsrapt.min.css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+            <h1>My Tasks</h1>
+
+            <ul class="list-group">
+                @foreach($tasks as $task)
+                    <li class="list-group-item">
+
+                        {{$task->body}}
+
+                    </li>
+                    @endforeach
+
+            </ul>
         </div>
     </body>
 </html>
